33db1404141fab2a31ef3829e2ce00802aded7d6
Sebastien Fortier <sebastien.fortier@ec.gc.ca>
2024-01-15 07:53:47 -0500

    fix python-rpn build

--- conda_recipies/python-rpn/conda.recipe/build.sh
+++ conda_recipies/python-rpn/conda.recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir $PREFIX/etc/
-cp -r $RECIPE_DIR/etc/* $PREFIX/etc/
+# mkdir $PREFIX/etc/
+# cp -r $RECIPE_DIR/etc/* $PREFIX/etc/
 
 # Install the Python package
 $PYTHON setup.py install
